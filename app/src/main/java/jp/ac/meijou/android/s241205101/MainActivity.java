a97be45e096e2c924a82ace2227d105642325463
Ryoga Seguchi <241205101@ccmailg.meijo-u.ac.jp>
2025-09-02 11:51:18 +0900

    ファイルの追加と変更

--- app/src/main/java/jp/ac/meijou/android/s241205101/MainActivity.java
+++ app/src/main/java/jp/ac/meijou/android/s241205101/MainActivity.java
@@ -8,17 +8,23 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import jp.ac.meijou.android.s241205101.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+        binding  = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        binding.text.setText(R.string.text);
     }
 }
