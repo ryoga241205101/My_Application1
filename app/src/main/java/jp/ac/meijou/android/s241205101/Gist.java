499dac363b994fed0ad188051d90a7c397dcdd31
Ryoga Seguchi <241205101@ccmailg.meijo-u.ac.jp>
2025-09-04 12:18:32 +0900

    day4 演習1

---
+++ app/src/main/java/jp/ac/meijou/android/s241205101/Gist.java
@@ -0,0 +1,11 @@
+package jp.ac.meijou.android.s241205101;
+
+import java.util.Map;
+
+public class Gist {
+    public Map<String, GistFile> files;
+
+    public static class GistFile {
+        public String content;
+    }
+}
